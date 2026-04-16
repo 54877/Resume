07fba43a1135a7d825eff61a42452a3bbd19a123
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-04-16 09:08:18 +0800

    新增TS技能

--- src/IndexApp.jsx
+++ src/IndexApp.jsx
@@ -341,7 +341,7 @@ function IndexApp() {
                     >
                       <path d="m344-60-76-128-144-32 14-148-98-112 98-112-14-148 144-32 76-128 136 58 136-58 76 128 144 32-14 148 98 112-98 112 14 148-144 32-76 128-136-58-136 58Zm34-102 102-44 104 44 56-96 110-26-10-112 74-84-74-86 10-112-110-24-58-96-102 44-104-44-56 96-110 24 10 112-74 86 74 84-10 114 110 24 58 96Zm102-318Zm-42 142 226-226-56-58-170 170-86-84-56 56 142 142Z" />
                     </svg>
-                    <span className="fs-7 fw-bold">JS</span>
+                    <span className="fs-7 fw-bold">JS / TS</span>
                   </div>
                   <div className="d-flex align-items-center w-p mb-3 ">
                     <svg
@@ -431,11 +431,11 @@ function IndexApp() {
               <div className="px-4">
                 <div className=" d-flex flex-column">
                   <ul className="pt-3 ul-type">
-                    {data.map((e) => {
+                    {data.map((e, index) => {
                       const { title, Url, UrlGithub, img, introduce, skill } =
                         e;
                       return (
-                        <div>
+                        <div key={index}>
                           <div className="d-none d-lg-block trapezoid-4"></div>
                           <div className="row" key={Url}>
                             <div className="col-lg-6">
